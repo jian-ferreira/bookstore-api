7c0c3e6f70d57e0dacae49bbeb9f1e2050befd44
jian-ferreira <jianferreiradev@gmail.com>
2023-03-30 17:35:33 -0300

    Primeiro commit: Subindo os arquivos do projeto para o servidor remoto

---
+++ src/main/java/com/jianferreira/bookstook/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.jianferreira.bookstook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
