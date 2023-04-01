989755472d14177010bbaad9278fd6fae325183b
jian-ferreira <jianferreiradev@gmail.com>
2023-04-01 17:55:27 -0300

    Finalizando as configurações da classe de domínio

--- src/main/java/com/jianferreira/bookstook/domain/Livro.java
+++ src/main/java/com/jianferreira/bookstook/domain/Livro.java
@@ -1,13 +1,29 @@
 package com.jianferreira.bookstook.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Livro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String autor;
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "categora_id")
 	private Categoria categoria;
 
 	public Livro() {
